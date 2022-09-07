d494b6495a15336436f035e7e79c21919abab34a
Lucas <lucas.michaudel@gmail.com>
2022-09-07 21:40:26 +0200

    bugfix

--- scripts/screens/2_title/SettingsScreen.gd
+++ scripts/screens/2_title/SettingsScreen.gd
@@ -1,5 +1,13 @@
 extends "res://scripts/ui/Screen.gd"
 
 func _show_screen(_info:Dictionary = {}):
+	ui_layer.show_back_button()
+
+func _setup_screen (_ui_layer: UILayer):
+	._setup_screen(_ui_layer)
+	
+	ui_layer.connect("back_button", self, "go_back")
+
+func go_back ():
 	if visible:
-		ui_layer.show_back_button()
+		ui_layer.show_screen("Title Screen")
